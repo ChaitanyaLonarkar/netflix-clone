afe5d573e6c4f166b287860009ecf494f80cf8fb
Chaitanya Lonarkar <chaitanyalonarkar@gmail.com>
2022-10-25 12:03:12 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let ans1 = document.getElementById("anser1");
+    let ans2 = document.getElementById("anser2");
+    let ans3 = document.getElementById("anser3");
+    let ans4 = document.getElementById("anser4");
+    let ans5 = document.getElementById("anser5");
+    let ans6 = document.getElementById("anser6");
+
+    function hideAns1() {
+        ans1.classList.toggle("hideAns")
+    }
+
+    function hideAns2() {
+        ans2.classList.toggle("hideAns")
+    }
+
+    function hideAns3() {
+        ans3.classList.toggle("hideAns")
+    }
+    function hideAns4() {
+        ans4.classList.toggle("hideAns")
+    }
+    function hideAns5() {
+        ans5.classList.toggle("hideAns")
+    }
+    function hideAns6() {
+        ans6.classList.toggle("hideAns")
+    }
+
